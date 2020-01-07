312b013c12fc37371bb81d359a5b340f88cde45f
todericamaxim <56884131+todericamaxim@users.noreply.github.com>
2020-01-07 17:50:42 +0200

    Add files via upload

---
+++ Tema/MyCalc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class MyCalc
+    {
+        public double Sum(double x,double y)
+        { return x + y; }
+        public double Sub(double x, double y)
+        { return x - y; }
+        public double Mul(double x, double y)
+        { return x * y; }
+        public double Div(double x, double y)
+        {
+            if (y==0)
+            {
+                throw new DivideByZeroException("Impossible to divide");
+            }
+            return x / y;
+        }
+    }
+}
